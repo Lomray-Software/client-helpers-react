ba28dfc73f9103089045c641f54807f5e70b2970
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-11-02 16:24:54 +0100

    fix: images type

--- typings/images.d.ts
+++ typings/images.d.ts
@@ -1,20 +1,20 @@
 declare module '*.png' {
-  const value: any;
+  const value: string;
   export default value;
 }
 
 declare module '*.jpg' {
-  const value: any;
+  const value: string;
   export default value;
 }
 
 declare module '*.jpeg' {
-  const value: any;
+  const value: string;
   export default value;
 }
 
 declare module '*.webp' {
-  const value: any;
+  const value: string;
   export default value;
 }
 
