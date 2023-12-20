671880714305367f17a1cd03b4c5b31a0039b2f9
Alexey <alexeidanilovich031193@gmail.com>
2023-12-20 21:05:50 +0300

    feat: modals migrated and removed (#14)

--- src/modals/root/create-modal-ref.ts
+++
@@ -1,8 +0,0 @@
-import type { IModalHookRef } from './types';
-
-const createModalRef = <TProps extends object>(): IModalHookRef<TProps> => ({
-  open: () => undefined,
-  hide: () => undefined,
-});
-
-export default createModalRef;
